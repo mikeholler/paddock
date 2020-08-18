ddaf1c9c075a0bace1908781dfe18fb6109a402f
Mike Holler <mike@mikeholler.me>
2020-08-17 22:18:18 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 import os
 import setuptools
+from codecs import open
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
+print(os.path.join(here, "paddock", "__version__.py"))
 with open(os.path.join(here, 'paddock', '__version__.py'), 'r', 'utf-8') as f:
     exec(f.read(), about)
 
 setuptools.setup(
-    name="paddock",
-    version="0.1.0",
-    description="iRacing Web SDK based off of ir_webstats",
-    url="https://github.com/mikeholler/paddock",
-    author="Mike Holler",
-    license="MIT",
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    url=about["__url__"],
+    author=about["__author__"],
+    email=about["__author_email__"],
+    license=about["__license__"],
     packages=setuptools.find_packages(
         exclude=[
             "tests", "*.tests", "*.tests.*",
