7c6efda20552ffd0e911a40fbf38952d5d446fec
Mike Holler <mike@mikeholler.me>
2020-08-17 22:08:35 -0500

    add publish to pypi workflow

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
 import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, 'paddock', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
 
 setuptools.setup(
     name="paddock",
@@ -7,7 +14,12 @@ setuptools.setup(
     url="https://github.com/mikeholler/paddock",
     author="Mike Holler",
     license="MIT",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(
+        exclude=[
+            "tests", "*.tests", "*.tests.*",
+            "integration_tests", "*.integration_tests", "*.integration_tests.*",
+        ]
+    ),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
