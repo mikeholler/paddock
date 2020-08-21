df98abc7964debbda032accaa4bb9b691eeb7c0f
Mike Holler <mike@mikeholler.me>
2020-08-21 17:18:48 -0500

    Improve documentation

--- paddock/models/_season.py
+++ paddock/models/_season.py
@@ -72,9 +72,9 @@ class SeasonTrack(JsonModelBaseclass):
 
     race_week: int = field(metadata={"data_key": "raceweek"})
     """
-    Race week is 0-indexed, with the first race week being week 0.
+    Race week is 1-indexed, with the first race week being week 1.
 
-    In a normal 12-week series, the range of values is 0..11, inclusive
+    In a normal 12-week series, the range of values is 1..12, inclusive
     on both ends.
     """
 
@@ -96,7 +96,20 @@ class SeasonTrack(JsonModelBaseclass):
 @dataclass
 class Season(JsonModelBaseclass):
     series_id: int = field(metadata={"data_key": "seriesid"})
+    """
+    Unique identifier for the series.
+
+    Series are repeated across multiple seasons.
+    """
+
     season_id: int = field(metadata={"data_key": "seasonid"})
+    """
+    Unique identifier for this particular season of this series.
+
+    Two different series will never have the same season ID. Likewise,
+    the same series may be present in a list, but never the same season of
+    the series.
+    """
 
     year: int = field(metadata={"data_key": "year"})
     """
@@ -108,33 +121,87 @@ class Season(JsonModelBaseclass):
     Quarter of the year. Legal values: 1, 2, 3, 4.
     """
 
-    # TODO: is this current race week at the time of the request?
-    # The completed series all have the maximum week, but maybe in progress
-    # seasons are different?
     race_week: int = field(metadata={"data_key": "raceweek"})
+    """
+    Race week is 0-indexed, with the first race week being week 0.
+    
+    IMPORTANT: This value is different from season race week, which is
+               1-indexed. Probably it's because this field is only used for
+               sorting, where the season race week is used for user interfaces.
+    
+    In a normal 12-week series, the range of values is 0..11, inclusive
+    on both ends. It's literally the index of the track in the tracks list.
+    """
 
     series_name: str = field(metadata={
         "marshmallow_field": QuotedStringField(data_key="seriesshortname")
     })
+    """
+    Human-readable name for the series.
+    """
 
     starts_at: dt.datetime = field(metadata={
         "marshmallow_field": UtcMillisecondDateTime(data_key="start"),
     })
+    """
+    Time when the series starts, in UTC.
+    """
+
     ends_at: dt.datetime = field(metadata={
         "marshmallow_field": UtcMillisecondDateTime(data_key="end"),
     })
+    """
+    Time when the series ends, in UTC.
+    """
 
     is_active: bool = field(metadata={"data_key": "active"})
+    """
+    Whether or not this season is currently between the start and end times.
+    """
+
     is_lite: bool = field(metadata={"data_key": "islite"})
+    """
+    It is unknown what this field means. No seasons are currently reporting
+    True at the time this code was written.
+    """
 
     raw_license: int = field(metadata={"data_key": "serieslicgroupid"})
-    raw_category: int = field(metadata={"data_key": "catid"})
+    """
+    Raw license integer value stored by iRacing servers.
 
-    category: Optional[int] = field(init=False)
-    license: Optional[int] = field(init=False)
+    Consider using license instead..
+    """
+
+    raw_category: int = field(metadata={"data_key": "catid"})
+    """
+    Raw category integer value stored by iRacing servers.
+
+    Consider using category instead..
+    """
+
+    category: Optional[Category] = field(init=False)
+    """
+    Friendly category (e.g., road).
+
+    None if raw_category is not able to be mapped to a Category.
+    """
+
+    license: Optional[License] = field(init=False)
+    """
+    Friendly license (e.g., A).
+
+    None if raw_license is not able to be mapped to a License.
+    """
 
     tracks: List[SeasonTrack]
+    """
+    List of tracks in this season of this series.
+    """
+
     cars: List[SeasonCar]
+    """
+    List of cars allowable in this season of this series.
+    """
 
     def __post_init__(self):
         self.license = maybe_value_of(License, self.raw_license)
